6587d7a26d38b79fc03bab650b3b4e8d6be08854
tboonma <tboonma@hotmail.com>
2023-02-25 13:59:59 +0700

    added db fixture

---
+++ example-1/fixtures/advertising/products.js
@@ -0,0 +1,34 @@
+const mongodb = require('mongodb')
+
+module.exports = [
+  {
+    _id: mongodb.ObjectId('63f9b11b898e63160b0d8380'),
+    name: 'Agoda',
+    url: 'https://www.agoda.com'
+  },
+  {
+    _id: mongodb.ObjectId('63f9b120898e63160b0d8391'),
+    name: 'Apple',
+    url: 'https://www.apple.com'
+  },
+  {
+    _id: mongodb.ObjectId('63f9b16d898e63160b0d8382'),
+    name: 'Sony',
+    url: 'https://www.sony.co.th'
+  },
+  {
+    _id: mongodb.ObjectId('63f9b16d898e63160b0d8383'),
+    name: 'Dell',
+    url: 'https://www.dell.com'
+  },
+  {
+    _id: mongodb.ObjectId('63f9b16d898e63160b0d8384'),
+    name: 'Samsung',
+    url: 'https://www.samsung.com'
+  },
+  {
+    _id: mongodb.ObjectId('63f9b16d898e63160b0d8385'),
+    name: 'Netflix',
+    url: 'https://www.netflix.com'
+  }
+]
